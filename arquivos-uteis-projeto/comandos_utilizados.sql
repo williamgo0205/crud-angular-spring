795d02a58867b016da3bde3c1b4d1ce022035530
William Goncalves de Oliveira <williamgo0205@gmail.com>
2022-05-15 15:20:16 -0300

    Adding initial commit project crud-angular-spring

---
+++ arquivos-uteis-projeto/comandos_utilizados.sql
@@ -0,0 +1,27 @@
+-- Comandos Curso Angula (LOIANE)
+
+--------------------------------
+-- ## REFERNCIAS DOCUMENTACAO --
+--------------------------------
+-- Angular Material
+link: https://material.angular.io/
+
+
+-----------------------------
+-- ## 1 - COMANDOS ANGULAR --
+-----------------------------
+
+-- Instalar angular
+-- No "cmd" após instalação do NodeJs, digitar o comando abaixo para instalar o Angular:
+ npm install -g @angular/cli
+
+-- Criar um projeto novo
+-- Na pasta do projeto digitar o seguinte comando:
+ ng new <nome-do-projeto>
+ ex: ng new crud-spring
+
+ -- Iniciar o servidor
+ ng serve
+
+ -- Instalando biblioteca angular material
+ ng add @angular/material
