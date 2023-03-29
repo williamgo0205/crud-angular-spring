035c2b12565147a6557c1d1d14db6129b8d85d19
William Goncalves de Oliveira <williamgo0205@gmail.com>
2023-03-29 11:04:39 -0300

    data(29/03/2023): Remove logica Soft Delete using anotations @SqlDelete and @Were

--- crud-spring/src/main/java/com/br/model/Course.java
+++ crud-spring/src/main/java/com/br/model/Course.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Column;
@@ -17,6 +19,10 @@ import javax.validation.constraints.Pattern;
 @Data
 @Entity
 @NoArgsConstructor
+// Anotação - @SQLDelete é possivel repassar o sql que queremos que o hibernate execute toda vez que ele executar o delete do registro
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+// Anotação - @Where é possivel filtrar no select where a clausula repassada, nesse caso "status = 'Ativo'
+@Where(clause = "status = 'Ativo'")
 //@Table(name = "cursos")
 public class Course {
 
@@ -38,4 +44,11 @@ public class Course {
     @Column(name = "category", length = 10, nullable = false)
     @JsonProperty("category")
     private String category;
+
+    @NonNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(name = "status", length = 10, nullable = false)
+    @JsonProperty("status")
+    private String status = "Ativo";
 }
